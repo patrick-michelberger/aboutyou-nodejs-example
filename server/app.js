faa96ce0a8640456668fee51a9ac8aedd6516188
patrick.michelberger <pmichelberger@gmx.de>
2014-12-02 16:48:59 +0100

    AY module is available in req object now

--- server/app.js
+++ server/app.js
@@ -9,9 +9,20 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
+
+// ABOUT YOU module
+var aboutYou = require('x-aboutyou-sdk')(100,'3ed93394c2b5ebd12c104b177b928ad0');
+
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+
+// Make the AboutYou module accessible to our router
+app.use(function(req,res,next){
+    req.aboutYou = aboutYou;
+    next();
+});
+
 require('./config/express')(app);
 require('./routes')(app);
 
