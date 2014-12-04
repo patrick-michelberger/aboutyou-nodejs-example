97fde2bfc239557dee346273a36dffa372fb2386
patrick.michelberger <pmichelberger@gmx.de>
2014-12-04 12:16:57 +0100

    api/apps implemented

--- server/api/app/app.controller.js
+++ server/api/app/app.controller.js
@@ -2,7 +2,12 @@
 
 var _ = require('lodash');
 
+var apps = require('../../config/app.settings.js');
+
 // Get list of apps
 exports.index = function(req, res) {
-  res.json([]);
+    var tokenFilter = _.map(apps.data, function(app) {
+        return _.omit(app, 'token');
+    });
+    res.json(tokenFilter);
 };
