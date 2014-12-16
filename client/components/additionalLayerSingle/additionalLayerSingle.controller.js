7dcb2ab23e98d1dedd3d45eb99c1646b98760fa6
patrick.michelberger <pmichelberger@gmx.de>
2014-12-16 13:46:17 +0100

    addItemToBasket feature added

--- client/components/additionalLayerSingle/additionalLayerSingle.controller.js
+++ client/components/additionalLayerSingle/additionalLayerSingle.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('aboutYouApp')
-    .controller('AdditionalLayerSingleCtrl', function ($scope, $modalInstance, product) {
+    .controller('AdditionalLayerSingleCtrl', function ($rootScope, $scope, $modalInstance, $window, product, appService) {
 
         // attributes
         $scope.product = product;
@@ -33,7 +33,26 @@ angular.module('aboutYouApp')
             $modalInstance.close();
         };
 
+        $scope.addItemToBasket = function (item) {
+            var currentApp = appService.getCurrentApp();
+
+            item = excludeItemId(item);
+
+            $window.AY.addToCart(item.id, item.quantity, 'MY_SESSION_ID', currentApp.selected.id, item.additional_data, function(error) {
+                $rootScope.$broadcast('basket:set:added');
+                $modalInstance.close();
+            });
+        };
+
         // helpers
+
+        var excludeItemId = function(item) {
+            return {
+                "id" : item.defaultVariant.id,
+                "quantity" : item.quantity,
+                "additional_data" : item.additional_data
+            };
+        };
 
         // HACK: watch for empty value keys
         $scope.$watch('product.additional_data', function(additionalData) {
